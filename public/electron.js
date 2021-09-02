412b0361ca2e52482c5dd987006f40ee81cd028a
AlMA PRO <alma.pro.leader@gmail.com>
2021-09-02 12:09:17 -0500

    feat(deps): adds devtron dep

--- public/electron.js
+++ public/electron.js
@@ -40,6 +40,7 @@ const createWindow = () => {
       .then(name => console.log(`Added Extension:  ${name}`))
       .catch(error => console.log(`An error occurred: , ${error}`));
   }
+  require('devtron').install();
 }
 
 app.whenReady().then(createWindow);
