24faf9d5ebb9451a35c668115058e054f5821791
AlMA PRO <alma.pro.leader@gmail.com>
2021-09-06 05:47:21 -0500

    refactor(public): moves devtron tools in development mode

--- public/electron.js
+++ public/electron.js
@@ -40,8 +40,8 @@ const createWindow = () => {
     installExtension(REACT_DEVELOPER_TOOLS)
       .then(name => console.log(`Added Extension:  ${name}`))
       .catch(error => console.log(`An error occurred: , ${error}`));
+    require('devtron').install();
   }
-  require('devtron').install();
 }
 
 app.whenReady().then(createWindow);
