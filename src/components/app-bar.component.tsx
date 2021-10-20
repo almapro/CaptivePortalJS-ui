36f7a44fe53a1ee407bf9719b92463f10fa5d544
AlMA PRO <alma.pro.leader@gmail.com>
2021-10-20 09:58:12 -0400

    refactor(components): migrates to @mui & refactors AppBar

--- src/components/app-bar.component.tsx
+++ src/components/app-bar.component.tsx
@@ -1,125 +1,56 @@
-import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import {
-	Menu as MenuIcon,
 	WbSunny as WbSunnyIcon,
 	Bedtime as BedtimeIcon,
-	Devices as DevicesIcon,
 	PowerOff as PowerOffIcon,
-	Settings as SettingsIcon,
-	Wifi as WifiIcon,
-	VpnKey as CredentialsIcon,
-	InsertDriveFile as HandshakeIcon,
-	Dashboard as DashboardIcon,
-	SettingsEthernet as SettingsEthernetIcon,
-} from '@material-ui/icons';
-import { useContext, useState } from 'react';
+} from '@mui/icons-material';
+import { useContext } from 'react';
 import { appContext } from '../App';
-import { Link } from 'react-router-dom';
 
 export type AppBarProps = {
 	title: string
 };
 
 export const AppBarComponent = ({ title }: AppBarProps) => {
-	const { darkMode, toggleDarkMode, setConnected, driver, setDriver, setDatabase, sigma, setSigma } = useContext(appContext);
+	const { darkMode, sigma, toggleDarkMode, setConnected, driver, setDriver, setDatabase } = useContext(appContext);
 	const disconnect = async () => {
 		await driver?.close();
-		sigma?.kill();
 		setDriver(null);
 		setDatabase('');
-		setSigma(null);
+		if (sigma) {
+			sigma.getGraph().clear();
+			sigma.listeners('rightClickNode').forEach(listener => sigma.removeListener('rightClickNode', listener as any));
+			sigma.getMouseCaptor().listeners('mousemove').forEach(listener => sigma.getMouseCaptor().removeListener('mousemove', listener as any));
+		}
 		setConnected(false);
 	}
-	const [open, setOpen] = useState(false);
-	const toggleOpen = (open: boolean) => (event: any) => {
-		if (
-			event &&
-			event.type === 'keydown' &&
-			((event as KeyboardEvent).key === 'Tab' || (event as KeyboardEvent).key === 'Shift')
-		) {
-			return;
-		}
-		setOpen(open);
-	}
-	const links: [JSX.Element, string, string][] = [
-		[<DashboardIcon />, '', 'Dashboard'],
-		[<DevicesIcon />, 'clients', 'Clients'],
-		[<WifiIcon />, 'wifis', 'Wi-Fi networks'],
-		[<SettingsEthernetIcon />, 'lans', 'LAN networks'],
-		[<HandshakeIcon />, 'handshakes', 'Handshakes'],
-		[<CredentialsIcon />, 'credentials', 'Credentials'],
-		[<SettingsIcon />, 'settings', 'Settings']
-	];
 	return (
-		<>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						onClick={() => setOpen(true)}
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						{title}
-					</Typography>
-					<IconButton
-						sx={{ margin: '0 15px' }}
-						color='inherit'
-						edge='end'
-						onClick={() => toggleDarkMode()}>
-						{
-							!darkMode ?
-							<WbSunnyIcon /> :
-							<BedtimeIcon />
-						}
-					</IconButton>
-					<IconButton
-						color='inherit'
-						edge='end'
-						onClick={() => disconnect()}>
-						<PowerOffIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<SwipeableDrawer
-				anchor='left'
-				open={open}
-				onClose={toggleOpen(false)}
-				onOpen={toggleOpen(true)}>
-					<Box
-						sx={{ width: 250 }}
-						role="presentation"
-						onClick={toggleOpen(false)}
-						onKeyDown={toggleOpen(false)}>
-							<List>
-		{
-				links.map((link, idx) => (
-								<ListItem key={idx} button component={Link} to={`/${link[1]}`}>
-									<ListItemIcon>
-				{link[0]}
-									</ListItemIcon>
-									<ListItemText>
-										{link[2]}
-									</ListItemText>
-								</ListItem>
-			))
-		}
-							</List>
-					</Box>
-			</SwipeableDrawer>
-		</>
+		<AppBar position="static">
+			<Toolbar>
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					{title}
+				</Typography>
+				<IconButton
+					sx={{ margin: '0 15px' }}
+					color='inherit'
+					edge='end'
+					onClick={() => toggleDarkMode()}>
+					{
+						!darkMode ?
+						<WbSunnyIcon /> :
+						<BedtimeIcon />
+					}
+				</IconButton>
+				<IconButton
+					color='inherit'
+					edge='end'
+					onClick={() => disconnect()}>
+					<PowerOffIcon />
+				</IconButton>
+			</Toolbar>
+		</AppBar>
 	);
 }
