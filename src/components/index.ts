61b9501587bb4a0e2840d3625bd920ae41f38907
AlMA PRO <alma.pro.leader@gmail.com>
2021-10-20 09:53:23 -0400

    feat(components): adds export entries to indexes

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from './app-bar.component';
 export * from './restricted-route.component';
 export * from './auth-route.component';
 export * from './context-menu.component';
+export * from './floating-actions.components';
+export * from './add-node.component';
+export * from './add-node-components';
