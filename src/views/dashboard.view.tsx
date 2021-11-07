6fd2cac3385ee3eba9d575e56bc296c8a46df741
AlMA PRO <alma.pro.leader@gmail.com>
2021-11-06 21:49:44 -0400

    feat(views): improves dashboard by adding image type of nodes & replacing SVG text  images with svg files

--- src/views/dashboard.view.tsx
+++ src/views/dashboard.view.tsx
@@ -9,16 +9,25 @@ import { useCallback, useContext, useEffect, useState } from 'react';
 import { useTitle } from 'react-use';
 import Captor from 'sigma/core/captors/captor';
 import { Settings } from 'sigma/settings';
-import { MouseCoords, NodeDisplayData, PartialButFor } from 'sigma/types';
+import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
+import { MouseCoords, NodeDisplayData } from 'sigma/types';
 import { appContext } from '../App';
 import { AddNode, AttachWifiToBuilding, ContextMenu, FloatingActions, NodeType } from '../components';
-import SvgToDataURL from 'svg-to-dataurl';
-import { Attributes, NodeKey } from 'graphology-types';
-import { useSigma, useSetSettings, useLoadGraph } from 'react-sigma-v2';
+import { Attributes } from 'graphology-types';
+import { useSigma, useSetSettings, useLoadGraph, useRegisterEvents } from 'react-sigma-v2';
 import circlepack from 'graphology-layout/circlepack';
+import forceAtlas2 from 'graphology-layout-forceatlas2';
 import Graph from 'graphology';
 import { useSnackbar } from "notistack"
 import _ from 'lodash';
+import { SpringSupervisor } from '../layout-spring';
+import RouterSvgIcon from '../images/Router.svg';
+import WifiSvgIcon from '../images/Wifi.svg';
+import HotspotSvgIcon from '../images/Hotspot.svg';
+import ClientSvgIcon from '../images/Client.svg';
+import BuildingSvgIcon from '../images/Building.svg';
+import HouseSvgIcon from '../images/House.svg';
+import FloorSvgIcon from '../images/Floor.svg';
 
 export type ClickNode = {
 	node: string
@@ -36,41 +45,83 @@ export type NodeStructure = {
 
 export const DashboardView = () => {
 	useTitle('Dashboard - Captive Portal JS');
-	const { driver, setSigma, darkMode, theme, database, createDatabaseIndexesAndConstraints } = useContext(appContext);
+	const { driver, setSigma, theme, database, createDatabaseIndexesAndConstraints } = useContext(appContext);
 	const { enqueueSnackbar } = useSnackbar();
 	const sigma = useSigma();
 	setSigma(sigma);
+	/**
+	 * Drag'n'Drop
+	 */
+	useEffect(() => {
+		let isDragging = false;
+		let draggedNode: string | null = null;
+		sigma.on('downNode', e => {
+			isDragging = true;
+			draggedNode = e.node;
+			sigma.getGraph().setNodeAttribute(e.node, 'highlighted', true);
+			sigma.getCamera().disable();
+		});
+		sigma.getMouseCaptor().on("mousemove", (e) => {
+			if (!isDragging || !draggedNode) return;
+
+			// Get new position of node
+			const pos = sigma.viewportToGraph(e);
+
+			sigma.getGraph().setNodeAttribute(draggedNode, "x", pos.x);
+			sigma.getGraph().setNodeAttribute(draggedNode, "y", pos.y);
+		});
+		// On mouse up, we reset the autoscale and the dragging mode
+		sigma.getMouseCaptor().on("mouseup", () => {
+			if (draggedNode) {
+				sigma.getGraph().removeNodeAttribute(draggedNode, "highlighted");
+			}
+			isDragging = false;
+			draggedNode = null;
+			sigma.getCamera().enable();
+		});
+		// Disable the autoscale at the first down interaction
+		sigma.getMouseCaptor().on("mousedown", () => {
+			if (!sigma.getCustomBBox()) sigma.setCustomBBox(sigma.getBBox());
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 	const setSigmaSettings = useSetSettings();
 	const loadGraph = useLoadGraph();
 	const addNodeToGraph = (node: any, node_type: NodeType, graph: Graph) => {
-		const data: any = { node_type };
+		const data: any = { node_type, type: 'image' };
 		switch (node_type) {
 			case 'WIFI':
-				data.label = node.essid;
+				data.image = WifiSvgIcon;
+				data.label = `${node.essid} - ${node.bssid}`;
 				data.essid = node.essid;
 				data.bssid = node.bssid;
 				break;
 			case 'HOTSPOT':
-				data.label = node.essid;
+				data.image = HotspotSvgIcon;
+				data.label = `${node.essid} - ${node.bssid}`;
 				data.essid = node.essid;
 				data.bssid = node.bssid;
 				break;
 			case 'BUILDING':
 			case 'HOUSE':
+				data.image = node_type === 'BUILDING' ? BuildingSvgIcon : HouseSvgIcon;
 				data.label = node.name;
 				data.name = node.name;
 				break;
 			case 'FLOOR':
+				data.image = FloorSvgIcon;
 				data.label = `Floor ${node.number}`;
 				data.number = node.number;
 				break;
 			case 'ROUTER':
-				data.label = node.ip;
+				data.image = RouterSvgIcon;
+				data.label = `${node.ip} - ${node.mac}`;
 				data.ip = node.ip;
 				data.mac = node.mac;
 				break;
 			case 'CLIENT':
-				data.label = node.ip;
+				data.image = ClientSvgIcon;
+				data.label = node.mac ? `${node.ip} - ${node.mac}` : node.ip;
 				data.ip = node.ip;
 				data.mac = node.mac;
 				break;
@@ -146,10 +197,15 @@ export const DashboardView = () => {
 				addEdgeToGraph(router.id, wifi.id, 'BROADCASTS', graph);
 			});
 			circlepack.assign(graph, { hierarchyAttributes: ['node_type'] });
+			const sensibleSettings = forceAtlas2.inferSettings(graph);
+			forceAtlas2(graph, { iterations: 50, settings: sensibleSettings });
+			forceAtlas2.assign(graph, 50);
 			loadGraph(graph);
+			new SpringSupervisor(graph, { isNodeFixed: (n) => graph.getNodeAttribute(n, "highlighted") }).start();
 			await session.close();
 		}
 	}
+	const createGraphCallback = useCallback(createGraph, [loadGraph, driver,  database]);
 	const [menu, setMenu] = useState<{
 		show: boolean,
 		node: string,
@@ -163,55 +219,6 @@ export const DashboardView = () => {
 		y: 0,
 		items: [],
 	});
-	const labelRenderer = async (ctx: CanvasRenderingContext2D, data: PartialButFor<NodeDisplayData, "x" | "y" | "size" | "label" | "color">) => {
-		let node_type = '';
-		if (sigma) {
-			const WifiSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0zm0 0h24v24H0V0z" fill="none"/><path d="M1 9l2 2c4.97-4.97 13.03-4.97 18 0l2-2C16.93 2.93 7.08 2.93 1 9zm8 8l3 3 3-3c-1.65-1.66-4.34-1.66-6 0zm-4-4l2 2c2.76-2.76 7.24-2.76 10 0l2-2C15.14 9.14 8.87 9.14 5 13z"/></svg>`;
-			const BuildingSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M17 11V3H7v4H3v14h8v-4h2v4h8V11h-4zM7 19H5v-2h2v2zm0-4H5v-2h2v2zm0-4H5V9h2v2zm4 4H9v-2h2v2zm0-4H9V9h2v2zm0-4H9V5h2v2zm4 8h-2v-2h2v2zm0-4h-2V9h2v2zm0-4h-2V5h2v2zm4 12h-2v-2h2v2zm0-4h-2v-2h2v2z" /></svg>`;
-			const HouseSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" /></svg>`;
-			const RouterSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="m20.2 5.9.8-.8C19.6 3.7 17.8 3 16 3s-3.6.7-5 2.1l.8.8C13 4.8 14.5 4.2 16 4.2s3 .6 4.2 1.7zm-.9.8c-.9-.9-2.1-1.4-3.3-1.4s-2.4.5-3.3 1.4l.8.8c.7-.7 1.6-1 2.5-1 .9 0 1.8.3 2.5 1l.8-.8zM19 13h-2V9h-2v4H5c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-4c0-1.1-.9-2-2-2zM8 18H6v-2h2v2zm3.5 0h-2v-2h2v2zm3.5 0h-2v-2h2v2z" /></svg>`;
-			const FloorSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="m11.99 18.54-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z" /></svg>`;
-			const ClientSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M4 6h18V4H4c-1.1 0-2 .9-2 2v11H0v3h14v-3H4V6zm19 2h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z" /></svg>`;
-			const HotspotSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M12 11c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 2c0-3.31-2.69-6-6-6s-6 2.69-6 6c0 2.22 1.21 4.15 3 5.19l1-1.74c-1.19-.7-2-1.97-2-3.45 0-2.21 1.79-4 4-4s4 1.79 4 4c0 1.48-.81 2.75-2 3.45l1 1.74c1.79-1.04 3-2.97 3-5.19zM12 3C6.48 3 2 7.48 2 13c0 3.7 2.01 6.92 4.99 8.65l1-1.73C5.61 18.53 4 15.96 4 13c0-4.42 3.58-8 8-8s8 3.58 8 8c0 2.96-1.61 5.53-4 6.92l1 1.73c2.99-1.73 5-4.95 5-8.65 0-5.52-4.48-10-10-10z" /></svg>`;
-			node_type = sigma.getGraph().getNodeAttributes(data.key).node_type;
-			const { x, y } = data;
-			const img = new Image(24, 24);
-			let imgData = '';
-			switch (node_type) {
-				case 'WIFI':
-					imgData = SvgToDataURL(WifiSvgIcon);
-					break;
-				case 'CLIENT':
-					imgData = SvgToDataURL(ClientSvgIcon);
-					break;
-				case 'BUILDING':
-					imgData = SvgToDataURL(BuildingSvgIcon);
-					break;
-				case 'HOUSE':
-					imgData = SvgToDataURL(HouseSvgIcon);
-					break;
-				case 'FLOOR':
-					imgData = SvgToDataURL(FloorSvgIcon);
-					break;
-				case 'HOTSPOT':
-					imgData = SvgToDataURL(HotspotSvgIcon);
-					break;
-				case 'ROUTER':
-					imgData = SvgToDataURL(RouterSvgIcon);
-					break;
-			}
-			img.id = data.key;
-			img.src = imgData;
-			ctx.drawImage(img, x - 12, y - 12);
-		}
-		if (data.label) {
-			ctx.fillStyle = darkMode ? '#fff' : '#000';
-			ctx.fillText(data.label, data.x - data.size * 0.75, data.y + (data.size * 2));
-			if (node_type === 'WIFI' || node_type === 'HOTSPOT') ctx.fillText(sigma.getGraph().getNodeAttribute(data.key, 'bssid'), data.x - data.size * 2.5, data.y + (data.size * 2) + 15);
-			if (node_type === 'ROUTER' || node_type === 'CLIENT') ctx.fillText(sigma.getGraph().getNodeAttribute(data.key, 'mac'), data.x - data.size * 2.5, data.y + (data.size * 2) + 15);
-		}
-	}
-	const labelRendererCallback = useCallback(labelRenderer, [darkMode, sigma]);
 	const handleNodeRightClick = (e: ClickNode) => {
 		const nodeType: NodeType = sigma.getGraph().getNodeAttribute(e.node, 'node_type');
 		const items: [JSX.Element, string, (id: string) => void][] = [];
@@ -245,7 +252,7 @@ export const DashboardView = () => {
 							setShowAttachWifiToBuilding(true);
 							setToBeAttachedWifiId(id);
 						}
-						createGraph();
+						createGraphCallback();
 					}
 				}]);
 				items.push([<RouterIcon />, `${attachedToRouter ?  'Deattach from' : 'Attach to'} a router`, async id => {
@@ -258,7 +265,7 @@ export const DashboardView = () => {
 						} else {
 							//
 						}
-						createGraph();
+						createGraphCallback();
 					}
 				}]);
 				break;
@@ -269,7 +276,7 @@ export const DashboardView = () => {
 				await session.run('MATCH (n {id: $id}) OPTIONAL MATCH r = (n)--() DELETE r, n', { id });
 				enqueueSnackbar('Node deleted successfully', { variant: 'success' });
 				await session.close();
-				createGraph();
+				createGraphCallback();
 			}
 		}]);
 		setMenu({
@@ -280,32 +287,80 @@ export const DashboardView = () => {
 			items,
 		});
 	}
-	const handleNodeRightClickCallback = useCallback(handleNodeRightClick, [setMenu]);
+	const handleNodeRightClickCallback = useCallback(handleNodeRightClick, [setMenu, driver, sigma, enqueueSnackbar, createGraphCallback, database]);
+	const [hoveredNode, setHoveredNode] = useState<string | null>(null);
+	const registerEvents = useRegisterEvents();
 	useEffect(() => {
-		const nodeReducer = (__: NodeKey, data: Attributes): Partial<NodeDisplayData> => {
-			return {
-				...data,
-				size: 15,
-			}
-		}
+		const nodeReducer = (__: string, data: Attributes): Partial<NodeDisplayData> => ({
+			...data,
+			size: 15,
+		});
 		const settings: Partial<Settings> = {
+			defaultNodeType: 'image',
 			defaultEdgeColor: theme.palette.primary.main,
 			renderLabels: true,
 			renderEdgeLabels: true,
 			defaultNodeColor: theme.palette.secondary.main,
-			labelRenderer: labelRendererCallback,
-			hoverRenderer: () => {},
-			labelSize: 5,
+			labelSize: 10,
+			labelWeight: 'bold',
+			labelGridCellSize: 0,
+			edgeLabelSize: 10,
 			nodeReducer,
+			nodeProgramClasses: {
+				image: getNodeProgramImage(),
+			},
 		}
 		setSigmaSettings(settings);
 		sigma.addListener('rightClickNode', handleNodeRightClickCallback);
-		createGraph();
-	}, [sigma, labelRendererCallback, theme, handleNodeRightClickCallback]);
+		createGraphCallback();
+		registerEvents({
+			enterNode: e => setHoveredNode(e.node),
+			leaveNode: () => setHoveredNode(null),
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	useEffect(() => {
+		setSigmaSettings({
+			defaultNodeType: 'image',
+			defaultEdgeColor: theme.palette.primary.main,
+			renderLabels: true,
+			renderEdgeLabels: true,
+			defaultNodeColor: theme.palette.secondary.main,
+			labelSize: 10,
+			labelWeight: 'bold',
+			labelGridCellSize: 0,
+			edgeLabelSize: 10,
+			nodeProgramClasses: {
+				image: getNodeProgramImage(),
+			},
+			nodeReducer: (node, data) => {
+				const graph = sigma.getGraph();
+				const newData: Attributes = { ...data, highlighted: data.highlighted || false, size: 15 };
+				if (hoveredNode) {
+					if (node === hoveredNode || graph.neighbors(hoveredNode).includes(node)) {
+						newData.highlighted = true;
+					} else {
+						newData.color = `#121212`;
+						newData.highlighted = false;
+					}
+				}
+				return newData;
+			},
+			edgeReducer: (edge, data) => {
+				const graph = sigma.getGraph();
+				const newData = { ...data, hidden: false };
+				if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
+					newData.hidden = true;
+				}
+				return newData;
+			},
+		});
+	}, [hoveredNode, setSigmaSettings, sigma, theme]);
 	useEffect(() => {
 		if (driver) {
 			createDatabaseIndexesAndConstraints(driver.session({ database }));
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 	const [showAddNode, setShowAddNode] = useState(false);
 	const [showAttachWifiToBuilding, setShowAttachWifiToBuilding] = useState(false);
@@ -313,8 +368,8 @@ export const DashboardView = () => {
 	return (
 		<>
 			<FloatingActions showAddNode={() => setShowAddNode(true)} />
-			<AddNode onDone={createGraph} show={showAddNode} close={() => setShowAddNode(false)}/>
-			<AttachWifiToBuilding wifiId={toBeAttachedWifiId} onDone={createGraph} show={showAttachWifiToBuilding} close={() => { setShowAttachWifiToBuilding(false); setToBeAttachedWifiId(''); }}/>
+			<AddNode onDone={createGraphCallback} show={showAddNode} close={() => setShowAddNode(false)}/>
+			<AttachWifiToBuilding wifiId={toBeAttachedWifiId} onDone={createGraphCallback} show={showAttachWifiToBuilding} close={() => { setShowAttachWifiToBuilding(false); setToBeAttachedWifiId(''); }}/>
 			<ContextMenu open={menu.show} closeMenu={() => setMenu({ ...menu, show: false })} node={menu.node} x={menu.x} y={menu.y} items={menu.items} />
 		</>
 	)
