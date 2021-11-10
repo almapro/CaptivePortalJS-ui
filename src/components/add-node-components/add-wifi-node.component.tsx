9c6d3f2cbd7724d3d34624e54a90f05bacbb35b4
AlMA PRO <alma.pro.leader@gmail.com>
2021-11-10 17:29:41 -0500

    refactor(components): replaces useAdddWifiNode hook with AddWifiNode and adds eventemitter to the component

--- src/components/add-node-components/add-wifi-node.component.tsx
+++ src/components/add-node-components/add-wifi-node.component.tsx
@@ -3,27 +3,17 @@ import { Grid, Typography, TextField } from "@mui/material"
 import { makeStyles } from "@mui/styles"
 import { Neo4jError, Session } from "neo4j-driver"
 import { useSnackbar } from "notistack"
-import { ChangeEvent, createRef, FC, FormEvent, Ref, useContext, useState } from "react"
+import { ChangeEvent, FC, useContext, useState, useEffect } from "react"
 import { v4 } from "uuid"
 import { appContext } from "../../App"
+import EventEmitter from 'events';
 
 export type AddWifiNodeProps = {
 	onDone: () => void
-	formRef: Ref<HTMLFormElement>
+	eventEmitter: EventEmitter
 }
 
-export const useAddWifiNode = ({ onDone }: Omit<AddWifiNodeProps, 'formRef'>): [() => void, JSX.Element] => {
-	const formRef = createRef<HTMLFormElement>();
-	const callSubmit = () => {
-		const form = formRef.current;
-		if (form) {
-			form.requestSubmit();
-		}
-	}
-	return [callSubmit, <AddWifiNode formRef={formRef} onDone={onDone} />];
-}
-
-export const AddWifiNode: FC<AddWifiNodeProps> = ({ onDone, formRef }) => {
+export const AddWifiNode: FC<AddWifiNodeProps> = ({ onDone, eventEmitter }) => {
 	const { enqueueSnackbar } = useSnackbar();
 	const { darkMode, driver, database } = useContext(appContext);
 	const [essid, setEssid] = useState('');
@@ -47,36 +37,36 @@ export const AddWifiNode: FC<AddWifiNodeProps> = ({ onDone, formRef }) => {
 		setBssidError(!/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.test(e.target.value));
 		setBssid(e.target.value);
 	}
-	const createWifi = async (session: Session) => {
+	const createWifi = async (session: Session, essid: string, bssid: string, password: string) => {
 		const id = v4();
 		const txr = session.beginTransaction();
 		await txr.run(`CREATE (n:Wifi { id: $id, essid: $essid, bssid: $bssid${password !== '' ? ', password: $password' : ''} })`, _.assign({}, { id, essid, bssid }, password !== '' ? { password } : {}));
 		await txr.commit();
 		await session.close();
 	}
-	const handleOnSubmit = async (e: FormEvent) => {
-		e.preventDefault();
-		if (driver) {
-			try {
-				await createWifi(driver.session({ database }));
-				enqueueSnackbar('Wifi node added successfuly', { variant: 'success' });
-				onDone();
-			} catch(e) {
-				enqueueSnackbar((e as Neo4jError).message, { variant: 'error' });
+	useEffect(() => {
+		eventEmitter.on('ADD_WIFI_NODE', () => {
+			if (driver) {
+				createWifi(driver.session({ database }), essid, bssid, password)
+					.then(() => { enqueueSnackbar('Wifi node added successfuly', { variant: 'success' }); })
+					.catch(err => { enqueueSnackbar((err as Neo4jError).message, { variant: 'error' }); })
+					.finally(() => onDone());
 			}
+		});
+		return () => {
+			eventEmitter.removeAllListeners('ADD_WIFI_NODE');
 		}
-	}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [essid, bssid, password]);
 	return (
 		<>
 			<Grid item xs={12}>
 				<Typography variant='caption'>Wifi</Typography>
 			</Grid>
 			<Grid item xs={12} container>
-				<form ref={formRef} onSubmit={handleOnSubmit}>
-					<TextField required label='ESSID' className={classes.input} value={essid} onChange={e => setEssid(e.target.value)} />
-					<TextField required label='BSSID' placeholder='xx:xx:xx:xx:xx:xx' className={classes.input} error={bssidError} helperText={bssidError ? 'BSSID must be formatted properly' : ''} value={bssid} onChange={handleBssidChange} />
-					<TextField label='Password (optional)' className={classes.input} value={password} onChange={e => setPassword(e.target.value)} />
-				</form>
+				<TextField required label='ESSID' className={classes.input} value={essid} onChange={e => setEssid(e.target.value)} />
+				<TextField required label='BSSID' placeholder='xx:xx:xx:xx:xx:xx' className={classes.input} error={bssidError} helperText={bssidError ? 'BSSID must be formatted properly' : ''} value={bssid} onChange={handleBssidChange} />
+				<TextField label='Password (optional)' className={classes.input} value={password} onChange={e => setPassword(e.target.value)} />
 			</Grid>
 		</>
 	)
